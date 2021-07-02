a6caa07071b4fe7dc7d4f0429169e96bf06c65f4
Intiserahmed <ei.ahmed@live.iium.edu.my>
2021-07-02 18:18:27 +0600

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4e5508d3dfcc84aa6fb931d13a495cef",
-"/": "4e5508d3dfcc84aa6fb931d13a495cef",
-"main.dart.js": "664dbe276182da968f2fdb1d418599e0",
+"index.html": "80b523ce82a4fd87d455888c3d50bc66",
+"/": "80b523ce82a4fd87d455888c3d50bc66",
+"main.dart.js": "d53f29eaa48dc0a27c993db2cd5416a2",
 "manifest.json": "b98b32134826400d106a731393d584f3",
 "version.json": "2e2246db59ede7f67c5985f6e131e30f"
 };
